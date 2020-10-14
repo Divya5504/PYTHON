872f79736fbb1a15dd3f10ef6e64de7b76c902ae
Divya <66994552+Divya5504@users.noreply.github.com>
2020-10-14 14:08:07 +0530

    dataTypes.py

---
+++ dataTypes.py
@@ -0,0 +1,11 @@
+name = "divya"
+age = 28
+address = "hyderabad"
+print(name)
+print(age)
+print(address)
+print(type(name))  # type method provide which type of data it is
+print(type(age))
+print(type(address))
+print(type(2.0002))
+print(type(True))
